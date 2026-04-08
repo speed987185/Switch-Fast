aafa24282a39202d5d3e7b982cdddb6a19ba6e03
AK3 <mjlabishek123@gmail.com>
2026-04-08 22:22:27 +0545

    added a slow mo on dying and trying to add a death animation on a player when it touches enemy

--- Script/kill_zone.gd
+++ Script/kill_zone.gd
@@ -6,10 +6,13 @@ extends Area2D
 
 func _on_body_entered(_body: Node2D) -> void:
 	print(" you died silly")
+	Engine.time_scale = 0.5
+	_body.get_node("CollisionShape2D").queue_free()
 	dyingtime.start()
 	
 
 
 func _on_dyingtime_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
 	
