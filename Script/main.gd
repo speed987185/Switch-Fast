24a8f24a662fd783187c6c9bee458531ced87fa0
AK3 <mjlabishek123@gmail.com>
2026-04-08 15:55:29 +0545

    made a simple leevel added animated platforrms mainly the player switching mechanisam is working good and trying to make level so we need to switch player time to time

--- Script/main.gd
+++ Script/main.gd
@@ -1,11 +1,77 @@
 extends Node2D
 
+@onready var player1: Node2D = $Player      # Capital P - matches your scene
+@onready var player2: Node2D = $Player2
+@onready var timer: Timer = $Timer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var active_player: Node2D
 
+func _ready():
+	active_player = player1
+	
+	# player 1
+	player1.visible = true
+	player1.process_mode = Node.PROCESS_MODE_INHERIT
+	enable_camera(player1, true)
+	
+	# player_2 hiden
+	player2.visible = false
+	player2.process_mode = Node.PROCESS_MODE_DISABLED
+	enable_camera(player2, false)
+	
+	# Timer of 5 sec to use player 2
+	timer.wait_time = 5.0
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_timeout)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if active_player == player1:
+			switch_to_player2()
+
+func switch_to_player2():
+	var old_pos = player1.global_position
+	
+	# Hide and stop player1
+	player1.visible = false
+	player1.process_mode = Node.PROCESS_MODE_DISABLED
+	enable_camera(player1, false)
+	
+	#  teleport player_2 
+	player2.global_position = old_pos
+	player2.visible = true
+	player2.process_mode = Node.PROCESS_MODE_INHERIT
+	enable_camera(player2, true)
+	
+	active_player = player2
+	timer.start()
+
+func _on_timer_timeout():
+	var old_pos = player2.global_position
+	
+	# Hide and stop player_2
+	player2.visible = false
+	player2.process_mode = Node.PROCESS_MODE_DISABLED
+	enable_camera(player2, false)
+	
+	# Show player1 at position of player2
+	player1.global_position = old_pos
+	player1.visible = true
+	player1.process_mode = Node.PROCESS_MODE_INHERIT
+	enable_camera(player1, true)
+	
+	active_player = player1
+
+# make camera process stable
+func enable_camera(player_node: Node2D, enable: bool):
+	var cam = player_node.get_node_or_null("Camera2D")
+	if cam:
+		cam.enabled = enable
+	else:
+		cam = player_node.get_node_or_null("Camera")
+		if cam:
+			cam.enabled = enable
+	
+	
+	
+	
