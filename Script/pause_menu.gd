7387646a543af27a775703d0b3afbd5c5ba89023
AK3 <mjlabishek123@gmail.com>
2026-04-11 09:05:57 +0545

    working on a level menu

--- Script/pause_menu.gd
+++ Script/pause_menu.gd
@@ -13,8 +13,14 @@ func _input(event: InputEvent) -> void:
 		else:
 			visible = true
 			get_tree().paused = true
+
 func _on_resume_pressed() -> void:
 	visible = false
-	get_tree().paused = false
 	
-	
+func _on_reset_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_menu_pressed() -> void:
+	if get_tree(): 
+		get_tree().paused = false
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
